b248d033b58c0621f99cc733c0ff408437099de8
Steve Knodl <steve@sixtyfiveford.com>
2016-10-31 02:44:26 +0000

    Use a valid package name

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@ import PackageDescription
 
 let package = Package(
 
-    name: "I2CBus-swift",
+    name: "I2CBus_Swift",
     dependencies: [
 	.Package(url: "https://www.github.com/sixtyfiveford/Ci2c.swift.git", majorVersion: 1),
     ]
